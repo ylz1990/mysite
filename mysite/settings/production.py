fe2f751a6b949b2a09e46b16e0267949a19cfdfa
ylz1990 <44316824+ylz1990@users.noreply.github.com>
2019-09-08 11:21:24 +0800

    修改上传图片路径

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -149,7 +149,7 @@ STATICFILES_DIRS=[
 
 # media
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media').replace('\\', '/')
 
 # 配置ckeditor
 CKEDITOR_UPLOAD_PATH = 'upload/'
